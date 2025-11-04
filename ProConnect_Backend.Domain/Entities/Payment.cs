0048748126a6e39859a5d0b3fe7ebf43b70ff617
DnilsonAH <dnilson.achahuanco@tecsup.edu.pe>
2025-11-04 14:46:03 -0500

    Scaffolding 2.0

---
+++ ProConnect_Backend.Domain/Entities/Payment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProConnect_Backend.Domain.Entities;
+
+public partial class Payment
+{
+    public uint PaymentId { get; set; }
+
+    public uint SessionId { get; set; }
+
+    public decimal TotalAmount { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public string? Method { get; set; }
+
+    public DateTime PaymentDate { get; set; }
+
+    public virtual Session Session { get; set; } = null!;
+}
---
+++ ProConnect_Backend.Domain/Entities/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProConnect_Backend.Domain.Entities;
+
+public partial class User
+{
+    public uint UserId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string? PhoneNumber { get; set; }
+
+    public DateTime RegistrationDate { get; set; }
+
+    public string Role { get; set; } = null!;
+
+    public string? PhotoUrl { get; set; }
+
+    public virtual ProfessionalProfile? ProfessionalProfile { get; set; }
+
+    public virtual ICollection<Review> ReviewClients { get; set; } = new List<Review>();
+
+    public virtual ICollection<Review> ReviewProfessionals { get; set; } = new List<Review>();
+
+    public virtual ICollection<Session> SessionClients { get; set; } = new List<Session>();
+
+    public virtual ICollection<Session> SessionProfessionals { get; set; } = new List<Session>();
+
+    public virtual ICollection<Verification> Verifications { get; set; } = new List<Verification>();
+
+    public virtual ICollection<WeeklyAvailability> WeeklyAvailabilities { get; set; } = new List<WeeklyAvailability>();
+}
