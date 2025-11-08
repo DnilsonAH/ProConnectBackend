12a5b3def4b5e5b47adba3682e2739410e066c51
valeskaYcp <168563099+valeskaYcp@users.noreply.github.com>
2025-11-08 14:40:34 -0500

    Create login DTOs

---
+++ ProConnect_Backend.Application/DTOsResponse/LoginDTOs/LoginRequestDto.cs
@@ -0,0 +1,7 @@
+namespace ProConnect_Backend.Application.DTOsResponse.LoginDTOs;
+
+public class LoginRequestDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
